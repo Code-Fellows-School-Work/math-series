d8030290b3783be39b2374d06c57330fca21c078
evidad <errolvidad@gmail.com>
2024-01-09 15:14:33 -0800

    wrote lucas recursive function and it passes all lucas test cases

--- math-series/math_series/series.py
+++ math-series/math_series/series.py
@@ -9,3 +9,13 @@ def fibonacci(n):
     # recursive case
     # used ChatGPT to mathematically express fibonacci sequence
     return fibonacci(n-1) + fibonacci(n-2)
+
+def lucas(n):
+    # base case
+    if n == 0:
+        return 2
+    
+    if n == 1:
+        return 1
+    
+    return lucas(n-2) + lucas(n-1)
--- math-series/tests/test_series.py
+++ math-series/tests/test_series.py
@@ -1,58 +1,58 @@
 # start off my importing the py test
 import pytest 
 # from this module, import this/these function(s)
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas
 
 # Fibonacci numbers: [0, 1, 1, 2, 3, 5, 8, 13]
 
-def fib_test_one():
+def test_one_fib():
     actual = fibonacci(0)
     expected = 0
     assert actual == expected
 
-def fib_test_two():
+def test_two_fib():
     actual = fibonacci(1)
     expected = 1
     assert actual == expected
 
-def fib_test_three():
+def test_three_fib():
     actual = fibonacci(2)
     expected = 1
     assert actual == expected
 
-def fib_test_fourth():
+def test_fourth_fib():
     actual = fibonacci(3)
     expected = 2
     assert actual == expected
 
-def fib_test_fifth():
+def test_fifth_fib():
     actual = fibonacci(4)
     expected = 3
     assert actual == expected
 
 # Lucas numbers [2, 1, 3, 4, 7, 11, 18, 29]
 
-def luc_test_one():
+def test_one_luc():
     actual = lucas(0)
     expected = 2
     assert actual == expected
 
-def luc_test_two():
+def test_two_luc():
     actual = lucas(1)
     expected = 1
     assert actual == expected
 
-def luc_test_three():
+def test_three_luc():
     actual = lucas(2)
     expected = 3
     assert actual == expected
 
-def luc_test_four():
+def test_four_luc():
     actual = lucas(3)
     expected = 4
     assert actual == expected
 
-def luc_test_five():
+def test_five_luc():
     actual = lucas(4)
     expected = 7
     assert actual == expected
